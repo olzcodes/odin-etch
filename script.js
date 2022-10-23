597f9fe690589e10364a0c7125e59882e24023e4
Oliver L <oliver.lkm@gmail.com>
2022-10-23 12:09:53 +1100

    Add styling for range slider

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const rangeSlider = document.querySelector(".range-slider");
+const sliderControl = document.querySelector("#slider-control");
+const sliderValue = document.querySelector(".slider-value");
 
-const createTiles = function () {
-  for (i = 0; i < 16; i++) {
+const generateTiles = function (input) {
+  for (i = 0; i < input; i++) {
     sketchContainer.innerHTML += `<div class="column-${i + 1}"></div>`;
     let column = document.querySelector(`.column-${i + 1}`);
-    for (j = 0; j < 16; j++) {
+    for (j = 0; j < input; j++) {
       column.innerHTML += `<div class="tile column-${i + 1} row-${
         j + 1
       }"></div>`;
@@ -12,7 +15,11 @@ const createTiles = function () {
   }
 };
 
-createTiles();
+generateTiles(16);
+
+const deleteTiles = function () {
+  sketchContainer.innerHTML = ``;
+};
 
 let tiles = document.querySelectorAll(".tile");
 
@@ -21,3 +28,16 @@ tiles.forEach((tile) =>
     tile.classList.add("shaded");
   })
 );
+
+const getSliderValue = function () {
+  sliderValue.textContent = sliderControl.getAttribute("value");
+};
+
+getSliderValue();
+
+sliderControl.addEventListener("click", function () {
+  sliderControl.setAttribute("value", this.value);
+  getSliderValue();
+  deleteTiles();
+  generateTiles(this.value);
+});
